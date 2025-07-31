472fefc321d6ca21f1814b5fef6b5b0210381691
DesireAnunobi <desireanunobi@gmail.com>
2025-07-31 15:30:57 +0100

    updated frontend testing file

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -2,6 +2,7 @@ const path = require('path');
 const jsdom = require('jsdom');
 
 const { JSDOM } = jsdom;
+
 const renderDOM = async (filename) => {
     const filepath = path.join(process.cwd(), filename);
 
@@ -17,6 +18,13 @@ const renderDOM = async (filename) => {
     });
 };
 
+
 module.exports = {
     renderDOM
 };
+
+// write all test file names to ignore when running helper.js
+
+test('dummy test', () => {
+  expect(true).toBe(true);
+});
